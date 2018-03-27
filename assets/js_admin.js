b954e9ff6bb9a6155de9af8f9e181cd9696ee4e5
Zhuk47 <zhukovskiy47@gmail.com>
2018-03-27 11:48:56 +0300

    multiply pics adding

--- assets/js_admin.js
+++ assets/js_admin.js
@@ -3,9 +3,30 @@
  */
 
 jQuery(function ($) {
-    /*
-     * действия при нажатии на кнопку загрузки изображения
-     */
+
+    //массовая загрузка изображений
+    $('body').on("click", ".insert-media", function () {
+        var row = $('.inside div[class=zbox]:last').attr('id').slice(-1);
+        wp.media.editor.send.attachment = function (props, attachment) {
+            var source = attachment.url;
+            $('div#zbox' + row + ' img.imageboxsrc').attr('src', source);
+
+            if ($('.nicEdit-main div.zhbox' + row + '').length > 0) {
+                $('div.zhbox' + row + ' h2').remove();
+                $('div.zhbox' + row + '').append('<h2 id="image_title' + row + '" class="image_title' + row + '">' + $('input[name=image_title' + row + ']').val() + '</h2>');
+                $('div.zhbox' + row + ' figure').remove();
+                $('div.zhbox' + row + '').append('<figure><img id="imageboxsrc' + row + '" src="' + source + '"></figure>');
+            } else {
+                $('.nicEdit-main').append('<div class="zhbox' + row + '">');
+                $('div.zhbox' + row + '').append('<h2 id="image_title' + row + '" class="image_title' + row + '">' + $('input[name=image_title' + row + ']').val() + '</h2>');
+                $('div.zhbox' + row + '').append('<figure><img id="imageboxsrc' + row + '" src="' + source + '"></figure>');
+            }
+            $('#zhaddblock').click();
+            row++;
+        };
+    });
+
+    //действия при нажатии на кнопку загрузки изображения
     $('body').on("click", ".upload_image_button", function () {
         var send_attachment_bkp = wp.media.editor.send.attachment;
         var button = $(this);
@@ -13,20 +34,20 @@ jQuery(function ($) {
         var row = name.slice(-1);
 
         wp.media.editor.send.attachment = function (props, attachment) {
-            $('img#imageboxsrc'+row+'').attr('src', attachment.url);
-            $('input[name=imagebox_image'+row+']').val(attachment.id);
+            $('img#imageboxsrc' + row + '').attr('src', attachment.url);
+            $('input[name=imagebox_image' + row + ']').val(attachment.id);
 
             //добавляем в textarea
-            var src1 = $('#zbox'+row+' img#imageboxsrc'+row+'').attr('src');
+            var src1 = $('#zbox' + row + ' img#imageboxsrc' + row + '').attr('src');
             if ($('.nicEdit-main div.zhbox' + row + '').length > 0) {
                 $('div.zhbox' + row + ' h2').remove();
                 $('div.zhbox' + row + '').append('<h2 id="image_title' + row + '" class="image_title' + row + '">' + $('input[name=image_title' + row + ']').val() + '</h2>');
                 $('div.zhbox' + row + ' figure').remove();
-                $('div.zhbox' + row + '').append('<figure><img id="imageboxsrc'+row+'" src="'+src1+'"></figure>');
+                $('div.zhbox' + row + '').append('<figure><img id="imageboxsrc' + row + '" src="' + src1 + '"></figure>');
             } else {
                 $('.nicEdit-main').append('<div class="zhbox' + row + '">');
                 $('div.zhbox' + row + '').append('<h2 id="image_title' + row + '" class="image_title' + row + '">' + $('input[name=image_title' + row + ']').val() + '</h2>');
-                $('div.zhbox' + row + '').append('<figure><img id="imageboxsrc'+row+'" src="'+src1+'"></figure>');
+                $('div.zhbox' + row + '').append('<figure><img id="imageboxsrc' + row + '" src="' + src1 + '"></figure>');
             }
 
             //скрываем блоки с картинками в текстовом редакторе
@@ -45,14 +66,9 @@ jQuery(function ($) {
      */
     $('body').on("click", ".remove_image_button", function () {
         var row = ($(this).attr('id').slice(-1));
-        // var src = $(this).parent().prev().attr('data-src');
-        // $('img#imageboxsrc'+row+'').attr('src', '');
-        // $('input[name=imagebox_image'+row+']').val('');
-        $('div[id=zbox'+row+']').remove();
+        $('div[id=zbox' + row + ']').remove();
         // убрать картинку из формы записи
         $('div.zhbox' + row + '').remove();
-        // $('input[name=image_title' + row + ']').val('');
-        // $('input[name=image_src' + row + ']').val('');
     });
 
     //перемещение блоков с картинками
@@ -73,16 +89,17 @@ jQuery(function ($) {
         $('#imagebox .inside div[class^=zbox]').each(function () {
             array += $(this).attr('id').slice(-1);
         });
+
         var count = $('#imagebox .inside .zbox').length;
         var j = 1;
         for (var i = 0; i <= count; i++) {
             if ($(".autonumbering").is(":checked")) {
                 $('#zbox' + array[i] + '').prepend('<b>' + j + '. </b>');
-                $('.zhbox' + array[i] + ' h2').prepend(''+j+'. ');
+                $('.zhbox' + array[i] + ' h2').prepend('' + j + '. ');
             } else {
                 $('#zbox' + array[i] + ' b').remove();
                 var text = $('.zhbox' + array[i] + ' h2').text();
-                text = text.replace(''+j+'. ', '');
+                text = text.replace('' + j + '. ', '');
                 $('.zhbox' + array[i] + ' h2').text(text);
             }
             j++;
@@ -95,10 +112,10 @@ jQuery(function ($) {
         var title = $(this).val();
         if ($('.nicEdit-main div.zhbox' + row + '').length > 0) {
             $('.nicEdit-main .zhbox' + row + ' h2').remove();
-            $('.nicEdit-main .zhbox' + row + '').prepend('<h2 id="image_title' + row + '" class="image_title' + row + '">'+title+'</h2>');
+            $('.nicEdit-main .zhbox' + row + '').prepend('<h2 id="image_title' + row + '" class="image_title' + row + '">' + title + '</h2>');
         } else {
             $('.nicEdit-main').append('<div class="zhbox' + row + '">');
-            $('.nicEdit-main .zhbox' + row + '').prepend('<h2 id="image_title' + row + '" class="image_title' + row + '">'+title+'</h2>');
+            $('.nicEdit-main .zhbox' + row + '').prepend('<h2 id="image_title' + row + '" class="image_title' + row + '">' + title + '</h2>');
         }
         //скрываем блоки с картинками в текстовом редакторе
         $('.nicEdit-main div').hide();
@@ -107,17 +124,17 @@ jQuery(function ($) {
         var row = $(this).attr('name').slice(-1);
         var src = $(this).val();
         var site = src.split('//')[1].split('/')[0];
-        $('.zbox img#imageboxsrc'+row+'').attr('src', src);
+        $('.zbox img#imageboxsrc' + row + '').attr('src', src);
         if ($('.nicEdit-main div.zhbox' + row + '').length > 0) {
             $('.nicEdit-main .zhbox' + row + ' figure').remove();
             $('div.zhbox' + row + '').append('<figure>');
-            $('div.zhbox' + row + ' figure').append('<img id="imageboxsrc'+row+'" src="'+src+'">');
-            $('div.zhbox' + row + ' figure').append('<figcaption><a href="'+src+'" target="_blank">'+site+'</a></figcaption>');
+            $('div.zhbox' + row + ' figure').append('<img id="imageboxsrc' + row + '" src="' + src + '">');
+            $('div.zhbox' + row + ' figure').append('<figcaption><a href="' + src + '" target="_blank">' + site + '</a></figcaption>');
         } else {
             $('.nicEdit-main').append('<div class="zhbox' + row + '">');
             $('div.zhbox' + row + '').append('<figure>');
-            $('div.zhbox' + row + ' figure').append('<img id="imageboxsrc'+row+'" src="'+src+'">');
-            $('div.zhbox' + row + ' figure').append('<figcaption><a href="'+src+'" target="_blank">'+site+'</a></figcaption>');
+            $('div.zhbox' + row + ' figure').append('<img id="imageboxsrc' + row + '" src="' + src + '">');
+            $('div.zhbox' + row + ' figure').append('<figcaption><a href="' + src + '" target="_blank">' + site + '</a></figcaption>');
         }
         //скрываем блоки с картинками в текстовом редакторе
         $('.nicEdit-main div').hide();
@@ -133,7 +150,7 @@ jQuery(function ($) {
         });
         var num = $('.nicEdit-main div[class^=zhbox]').length;
         if (num > 1) {
-            for (var i = 0; i <= num-1; i++) {
+            for (var i = 0; i <= num - 1; i++) {
                 $('#zhaddblock').click();
                 var title = $('.zhbox' + array[i] + ' h2').text();
                 $('.zbox input[name=image_title' + i + ']').val(title);
