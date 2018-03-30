56c0af62de53207792a5b702b0d72efb5ae4001b
Zhuk47 <zhukovskiy47@gmail.com>
2018-03-30 15:15:52 +0300

    std redactor & textarea->div stuff

--- assets/js_admin.js
+++ assets/js_admin.js
@@ -6,26 +6,36 @@ jQuery(function ($) {
 
     //массовая загрузка изображений
     $('body').on("click", ".insert-media", function () {
+        // $('#zhaddblock').click();
         var row = $('.inside div[class=zbox]:last').attr('id').slice(-1);
+        var num = parseInt(row) + 1;
         wp.media.editor.send.attachment = function (props, attachment) {
             var source = attachment.url;
             $('div#zbox' + row + ' img.imageboxsrc').attr('src', source);
 
-            if ($('.nicEdit-main div.zhbox' + row + '').length > 0) {
-                $('div.zhbox' + row + ' h2').remove();
-                $('div.zhbox' + row + '').append('<h2 id="image_title' + row + '" class="image_title' + row + '">' + $('input[name=image_title' + row + ']').val() + '</h2>');
-                $('div.zhbox' + row + ' figure').remove();
-                $('div.zhbox' + row + '').append('<figure><img id="imageboxsrc' + row + '" src="' + source + '"></figure>');
+            if ($('div.zhbox' + row + '').length > 0) {
+                $('div#addblock div.textareadiv').append('<div class="zhbox' + row + '"></div>');
+                if ($(".autonumbering").is(":checked")) {
+                    $('div.zhbox' + row + '').append('<h2>' + num + '. ' + $('input[name=image_title' + row + ']').val() + '</h2>');
+                } else {
+                    $('div.zhbox' + row + '').append('<h2>' + $('input[name=image_title' + row + ']').val() + '</h2>');
+                }
+                $('div.zhbox' + row + '').append('<figure><img src="' + source + '"/><figcaption><a href="" target="_blank"></a></figcaption></figure>');
+                $('textarea.wp-editor-area').val($('div.textareadiv').html());
             } else {
-                $('.nicEdit-main').append('<div class="zhbox' + row + '">');
-                $('div.zhbox' + row + '').append('<h2 id="image_title' + row + '" class="image_title' + row + '">' + $('input[name=image_title' + row + ']').val() + '</h2>');
-                $('div.zhbox' + row + '').append('<figure><img id="imageboxsrc' + row + '" src="' + source + '"></figure>');
+                $('div#addblock div.textareadiv').append('<div class="zhbox' + row + '"></div>');
+                if ($(".autonumbering").is(":checked")) {
+                    $('div.zhbox' + row + '').append('<h2>' + num + '. ' + $('input[name=image_title' + row + ']').val() + '</h2>');
+                } else {
+                    $('div.zhbox' + row + '').append('<h2>' + $('input[name=image_title' + row + ']').val() + '</h2>');
+                }
+                $('div.zhbox' + row + '').append('<figure><img src="' + source + '"/><figcaption><a href="" target="_blank"></a></figcaption></figure>');
+                $('textarea.wp-editor-area').val($('div.textareadiv').html());
             }
-            //скрываем блоки с картинками в текстовом редакторе
-            $('.nicEdit-main div').hide();
 
             $('#zhaddblock').click();
             row++;
+            num++;
         };
     });
 
@@ -35,26 +45,22 @@ jQuery(function ($) {
         var button = $(this);
         var name = ($(this).attr('id'));
         var row = name.slice(-1);
+        var num = parseInt(row) + 1;
 
         wp.media.editor.send.attachment = function (props, attachment) {
             $('img#imageboxsrc' + row + '').attr('src', attachment.url);
             $('input[name=imagebox_image' + row + ']').val(attachment.id);
+            var source = attachment.url;
 
-            //добавляем в textarea
-            var src1 = $('#zbox' + row + ' img#imageboxsrc' + row + '').attr('src');
-            if ($('.nicEdit-main div.zhbox' + row + '').length > 0) {
-                $('div.zhbox' + row + ' h2').remove();
-                $('div.zhbox' + row + '').append('<h2 id="image_title' + row + '" class="image_title' + row + '">' + $('input[name=image_title' + row + ']').val() + '</h2>');
-                $('div.zhbox' + row + ' figure').remove();
-                $('div.zhbox' + row + '').append('<figure><img id="imageboxsrc' + row + '" src="' + src1 + '"></figure>');
+            $('div.zhbox' + row + '').remove();
+            $('div#addblock div.textareadiv').append('<div class="zhbox' + row + '"></div>');
+            if ($(".autonumbering").is(":checked")) {
+                $('div.zhbox' + row + '').append('<h2>' + num + '. ' + $('input[name=image_title' + row + ']').val() + '</h2>');
             } else {
-                $('.nicEdit-main').append('<div class="zhbox' + row + '">');
-                $('div.zhbox' + row + '').append('<h2 id="image_title' + row + '" class="image_title' + row + '">' + $('input[name=image_title' + row + ']').val() + '</h2>');
-                $('div.zhbox' + row + '').append('<figure><img id="imageboxsrc' + row + '" src="' + src1 + '"></figure>');
+                $('div.zhbox' + row + '').append('<h2>' + $('input[name=image_title' + row + ']').val() + '</h2>');
             }
-
-            //скрываем блоки с картинками в текстовом редакторе
-            $('.nicEdit-main div').hide();
+            $('div.zhbox' + row + '').append('<figure><img src="' + source + '"/><figcaption><a href="" target="_blank"></a></figcaption></figure>');
+            $('textarea.wp-editor-area').val($('div.textareadiv').html());
 
             wp.media.editor.send.attachment = send_attachment_bkp;
 
@@ -72,18 +78,21 @@ jQuery(function ($) {
         $('div[id=zbox' + row + ']').remove();
         // убрать картинку из формы записи
         $('div.zhbox' + row + '').remove();
+        $('textarea.wp-editor-area').val($('div.textareadiv').html());
     });
 
     //перемещение блоков с картинками
-    $('body').on('click', ".inside :button[value=down]", function () {
+    $('body').on('click', ".inside :button[id=down]", function () {
         $(this).closest('.zbox').insertAfter($(this).closest('.zbox').next());
         var row = $(this).prev().prev().attr('name').slice(-1);
         $('div.zhbox' + row + '').closest('div[class^=zhbox]').insertAfter($('div.zhbox' + row + '').closest('div[class^=zhbox]').next());
+        $('textarea.wp-editor-area').val($('div.textareadiv').html());
     });
-    $('body').on('click', ".inside :button[value=up]", function () {
+    $('body').on('click', ".inside :button[id=up]", function () {
         $(this).closest('.zbox').insertBefore($(this).closest('.zbox').prev());
         var row = $(this).prev().attr('name').slice(-1);
         $('div.zhbox' + row + '').closest('div[class^=zhbox]').insertBefore($('div.zhbox' + row + '').closest('div[class^=zhbox]').prev());
+        $('textarea.wp-editor-area').val($('div.textareadiv').html());
     });
 
     //автонумерация
@@ -113,58 +122,92 @@ jQuery(function ($) {
     $('.inside').on('input', '#image_title', function () {
         var row = $(this).attr('name').slice(-1);
         var title = $(this).val();
-        if ($('.nicEdit-main div.zhbox' + row + '').length > 0) {
-            $('.nicEdit-main .zhbox' + row + ' h2').remove();
-            $('.nicEdit-main .zhbox' + row + '').prepend('<h2 id="image_title' + row + '" class="image_title' + row + '">' + title + '</h2>');
+        if ($('div.textareadiv div.zhbox' + row + '').length > 0) {
+            $('div.zhbox' + row + ' h2').remove();
+            $('div.zhbox' + row + '').prepend('<h2>' + title + '</h2>');
+            $('textarea.wp-editor-area').val($('div.textareadiv').html());
         } else {
-            $('.nicEdit-main').append('<div class="zhbox' + row + '">');
-            $('.nicEdit-main .zhbox' + row + '').prepend('<h2 id="image_title' + row + '" class="image_title' + row + '">' + title + '</h2>');
+            $('div.textareadiv').append('<div class="zhbox' + row + '">');
+            $('div.zhbox' + row + ' h2').remove();
+            $('div.zhbox' + row + '').prepend('<h2>' + title + '</h2>');
+            $('textarea.wp-editor-area').val($('div.textareadiv').html());
         }
-        //скрываем блоки с картинками в текстовом редакторе
-        $('.nicEdit-main div').hide();
     });
     $('.inside').on('input', '#image_src', function () {
         var row = $(this).attr('name').slice(-1);
         var src = $(this).val();
         var site = src.split('//')[1].split('/')[0];
         $('.zbox img#imageboxsrc' + row + '').attr('src', src);
-        if ($('.nicEdit-main div.zhbox' + row + '').length > 0) {
-            $('.nicEdit-main .zhbox' + row + ' figure').remove();
-            $('div.zhbox' + row + '').append('<h2 id="image_title' + row + '" class="image_title' + row + '"></h2>');
+        if ($('div.zhbox' + row + '').length > 0) {
+            $('div.zhbox' + row + ' figure').remove();
+            $('div.zhbox' + row + '').append('<h2></h2>');
             $('div.zhbox' + row + '').append('<figure>');
-            $('div.zhbox' + row + ' figure').append('<img id="imageboxsrc' + row + '" src="' + src + '">');
+            $('div.zhbox' + row + ' figure').append('<img src="' + src + '">');
             $('div.zhbox' + row + ' figure').append('<figcaption><a href="' + src + '" target="_blank">' + site + '</a></figcaption>');
+            $('textarea.wp-editor-area').val($('div.textareadiv').html());
         } else {
-            $('.nicEdit-main').append('<div class="zhbox' + row + '">');
-            $('div.zhbox' + row + '').append('<h2 id="image_title' + row + '" class="image_title' + row + '"></h2>');
+            $('div.textareadiv').append('<div class="zhbox' + row + '">');
+            $('div.zhbox' + row + '').append('<h2></h2>');
             $('div.zhbox' + row + '').append('<figure>');
-            $('div.zhbox' + row + ' figure').append('<img id="imageboxsrc' + row + '" src="' + src + '">');
+            $('div.zhbox' + row + ' figure').append('<img src="' + src + '">');
             $('div.zhbox' + row + ' figure').append('<figcaption><a href="' + src + '" target="_blank">' + site + '</a></figcaption>');
+            $('textarea.wp-editor-area').val($('div.textareadiv').html());
         }
-        //скрываем блоки с картинками в текстовом редакторе
-        $('.nicEdit-main div').hide();
     });
 
     //отображение картинок поста в редакторе
     $(window).load(function () {
-        //скрываем блоки с картинками в текстовом редакторе
-        $('.nicEdit-main div').hide();
-        var array = [];
-        $('.nicEdit-main div[class^=zhbox]').each(function () {
-            array += $(this).attr('class').slice(-1);
-        });
-        var num = $('.nicEdit-main div[class^=zhbox]').length;
-        if (num > 0) {
-            for (var i = 0; i <= num - 1; i++) {
-                $('#zhaddblock').click();
-                var title = $('.zhbox' + array[i] + ' h2').text();
-                $('.zbox input[name=image_title' + i + ']').val(title);
-                var img = $('.zhbox' + array[i] + ' img').attr('src');
-                $('.zbox img[id=imageboxsrc' + i + ']').attr('src', img);
-                var src = $('.zhbox' + array[i] + ' div').text();
-                $('.zbox input[name=image_src' + i + ']').val(src);
-            }
+
+        //скрываем блоки с картинками в текстовом редакторе (не скрывается в iFrame походу)
+        // $('#tinymce').contents().getElementsByTagName('h2').hide();
+        // $('#tinymce').contents().getElementsByTagName('figure').hide();
+
+        // var iframe = frame.contentWindow.document.getElementById('tinymce');
+        // iframe.document.getElementsByTagName('h2').style.display = 'none';
+        // iframe.document.getElementsByTagName('figure').style.display = 'none';
+
+        // $('iframe h2').hide();
+        // $('iframe figure').hide();
+
+        var images = [];
+        var text = ($('textarea.wp-editor-area').val());
+        $('#addblock').append('<div hidden class="textareadiv"></div>');
+        $('div.textareadiv').html(text);
+
+        var elems = $("<div/>", {html: text}).find("h2");
+        var figures = $("<div/>", {html: text}).find("figure");
+        var length = $("<div/>", {html: text}).find("h2").length;
+        var i = 0;
+        for (i; i < length; i++) {
+            $('#zhaddblock').click();
+            $('.zbox input[name=image_title' + i + ']').val(elems[i].innerHTML);
+            images.push(figures[i].innerHTML);
+            var src = images[i].split('"><fig')[0].split('src="')[1];
+            $('.zbox img[id=imageboxsrc' + i + ']').attr('src', src);
         }
-    })
+
+    });
+
+    //добавление блока
+    var rowCheck = 1;
+    $("body").on("click", "#zhaddblock", function () {
+        if (!$(".autonumbering").is(":checked")) {
+            var html = '<div class="zbox" id="zbox' + rowCheck + '"><input id="image_title" name="image_title' + rowCheck + '" placeholder="Заголовок картинки"/> <input type="button" id="up" value="&#xf106" class="fa fa-angle-up"> <input type="button" id="down" value="&#xf107" class="fa fa-angle-down"> <div></br></div><img class="imageboxsrc" id="imageboxsrc' + rowCheck + '" src=""/><div><input type="hidden" class="button imagebox_image" name="imagebox_image' + rowCheck + '" value=""/><div id="zhbut"><button type="button" id="insert-media-button' + rowCheck + '" class="upload_image_button button insert-media add_media">Вставить из библиотеки</button> <button type="button" id="zhremove' + rowCheck + '" class="remove_image_button button add_media">Удалить изображение</button></div></div><div></br></div><input id="image_src" name="image_src' + rowCheck + '" placeholder="Ссылка"/></div>';
+            $(html).appendTo($("#imagebox .inside"));
+            rowCheck++;
+        }
+    });
+
+    $("body").on("click", "#zhaddblock", function () {
+        $(window).ready(function () {
+            var rowUnCheck = $('.inside .zbox').length;
+            var rowUnCheckNum = $('.inside .zbox').length + 1;
+            if ($(".autonumbering").is(":checked")) {
+                var html = '<div class="zbox" id="zbox' + rowUnCheck + '"><b>' + rowUnCheckNum + '. </b><input id="image_title" name="image_title' + rowUnCheck + '" placeholder="Заголовок картинки"/> <input type="button" id="up" value="&#xf106" class="fa fa-angle-up"> <input type="button" id="down" value="&#xf107" class="fa fa-angle-down"> <div></br></div><img class="imageboxsrc" id="imageboxsrc' + rowUnCheck + '" src=""/><div><input type="hidden" class="button imagebox_image" name="imagebox_image' + rowUnCheck + '" value=""/><div id="zhbut"><button type="button" id="insert-media-button' + rowUnCheck + '" class="upload_image_button button insert-media add_media">Вставить из библиотеки</button> <button type="button" id="zhremove' + rowUnCheck + '" class="remove_image_button button add_media">Удалить изображение</button></div></div><div></br></div><input id="image_src" name="image_src' + rowUnCheck + '" placeholder="Ссылка"/></div>';
+                $(html).appendTo($("#imagebox .inside"));
+                rowUnCheck++;
+            }
+        });
+    });
 
 });
--- includes/admin.php
+++ includes/admin.php
@@ -8,21 +8,44 @@
 
 
 // убираем стандартный editor
-function zh_post_editor_remove_editor()
+//function zh_post_editor_remove_editor()
+//{
+//    remove_post_type_support('post', 'editor');
+//}
+//
+//add_action('init', 'zh_post_editor_remove_editor');
+
+function RemoveAddMediaButton()
 {
-    remove_post_type_support('post', 'editor');
+    remove_action('media_buttons', 'media_buttons');
 }
 
-add_action('init', 'zh_post_editor_remove_editor');
+add_action('admin_init', 'RemoveAddMediaButton');
+
+function removeButtonsFromTinyMCE($toolbars) {
+    $disableButtons = array('bullist', 'numlist', 'blockquote', 'alignleft', 'alignright', 'aligncenter', 'wp_more', 'wp_adv');
+    $temporary = explode(',', $toolbars['toolbar1']);
+    foreach ($disableButtons as $disableButton) {
+        if (($key = array_search($disableButton, $temporary)) !== false) {
+            unset($temporary[$key]);
+        }
+    }
+    $toolbars['toolbar1'] = implode(',', array_values($temporary));
+    return $toolbars;
+}
+
+add_filter('tiny_mce_before_init', 'removeButtonsFromTinyMCE');
 
 // добавляем свой editor
 function zh_post_editor_add_editor()
 {
     wp_enqueue_script("jquery");
-    wp_enqueue_script('nicEdit', plugins_url('zh_post_editor/assets/nicEdit/nicEdit.js'));
+//    wp_enqueue_script('nicEdit', plugins_url('zh_post_editor/assets/nicEdit/nicEdit.js'));
     wp_enqueue_script('myuploadscript', plugins_url('zh_post_editor/assets/js_admin.js'));
-    wp_enqueue_style('nicEditGif', plugins_url('zh_post_editor/assets/nicEdit/nicEditorIcons.gif'));
+//    wp_enqueue_style('nicEditGif', plugins_url('zh_post_editor/assets/nicEdit/nicEditorIcons.gif'));
     wp_enqueue_style('zhstyle', plugins_url('zh_post_editor/assets/nicEdit/zhstyle.css'));
+    wp_enqueue_style('fawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+
 }
 
 add_action('admin_enqueue_scripts', 'zh_post_editor_add_editor');
@@ -32,7 +55,7 @@ add_action('admin_enqueue_scripts', 'zh_post_editor_add_editor');
 function zh_post_editor_meta_box()
 {
     add_meta_box('autonumbering', 'Автонумерация', 'zh_post_editor_print_box', 'post', 'side', 'high');
-    add_meta_box('textarea', 'Вступительный текст', 'zh_post_editor_print_textarea_box', 'post', 'normal', 'high');
+//    add_meta_box('textarea', 'Вступительный текст', 'zh_post_editor_print_textarea_box', 'post', 'normal', 'high');
     add_meta_box('addblock', 'Добавление блоков', 'zh_post_editor_print_addblock_box', 'post', 'advanced', 'high');
     add_meta_box('imagebox', 'Блок изображения', 'zh_post_editor_print_imagebox_box', 'post', 'normal', 'high');
 }
@@ -51,16 +74,16 @@ function zh_post_editor_print_box($post)
 
 
 // вступительный текст
-function zh_post_editor_print_textarea_box($post)
-{
-    wp_nonce_field(basename(__FILE__), 'seo_textarea_metabox_nonce');
-    $html = '';
-    $html .= '<label><textarea id="zhTextarea" class="zhTextarea" name="post_content" style="width: 99%; height: 300px">';
-    $html .= get_post_field('post_content', $post->ID, 'edit');
-    $html .= '</textarea></label>';
-    $html .= '<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>';
-    echo $html;
-}
+//function zh_post_editor_print_textarea_box($post)
+//{
+//    wp_nonce_field(basename(__FILE__), 'seo_textarea_metabox_nonce');
+//    $html = '';
+//    $html .= '<label><textarea id="zhTextarea" class="zhTextarea" name="post_content" style="width: 99%; height: 300px">';
+//    $html .= get_post_field('post_content', $post->ID, 'edit');
+//    $html .= '</textarea></label>';
+//    $html .= '<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>';
+//    echo $html;
+//}
 
 // добавление блоков
 function zh_post_editor_print_addblock_box($post)
@@ -71,14 +94,14 @@ function zh_post_editor_print_addblock_box($post)
     $html .= '<label><button type="button" id="zhaddblock" name="addblock" class="button">Добавить блок</button>';
     $html .= ' ';
     $html .= '<button type="button" id="insert-media-button" class="button insert-media add_media">Загрузить картинки</button></label>';
-    $html .= '<script>
-	    var row = 1;
-	    jQuery("body").on("click", "#zhaddblock", function() {
-            var html = `<div class="zbox" id="zbox`+row+`"><input id="image_title" name="image_title`+row+`" placeholder="Заголовок картинки"/> <input type="button" value="up"> <input type="button" value="down"> <div></br></div><img class="imageboxsrc" id="imageboxsrc`+row+`" src=""/><div><input type="hidden" class="button imagebox_image" name="imagebox_image`+row+`" value=""/><div id="zhbut"><button type="button" id="insert-media-button`+row+`" class="upload_image_button button insert-media add_media">Вставить из библиотеки</button> <button type="button" id="zhremove`+row+`" class="remove_image_button button add_media">Удалить изображение</button></div></div><div></br></div><input id="image_src" name="image_src`+row+`" placeholder="Ссылка"/></div>`;
-            $(html).appendTo($("#imagebox .inside"));
-            row++;
-        });
-    </script>';
+//    $html .= '<script>
+//	    var row = 1;
+//	    jQuery("body").on("click", "#zhaddblock", function() {
+//            var html = `<div class="zbox" id="zbox`+row+`"><input id="image_title" name="image_title`+row+`" placeholder="Заголовок картинки"/> <input type="button" id="up" value="&#xf106" class="fa fa-angle-up"> <input type="button" id="down" value="&#xf107" class="fa fa-angle-down"> <div></br></div><img class="imageboxsrc" id="imageboxsrc`+row+`" src=""/><div><input type="hidden" class="button imagebox_image" name="imagebox_image`+row+`" value=""/><div id="zhbut"><button type="button" id="insert-media-button`+row+`" class="upload_image_button button insert-media add_media">Вставить из библиотеки</button> <button type="button" id="zhremove`+row+`" class="remove_image_button button add_media">Удалить изображение</button></div></div><div></br></div><input id="image_src" name="image_src`+row+`" placeholder="Ссылка"/></div>`;
+//            $(html).appendTo($("#imagebox .inside"));
+//            row++;
+//        });
+//    </script>';
     echo $html;
 }
 
@@ -90,14 +113,14 @@ function zh_post_editor_print_imagebox_box($post)
     wp_nonce_field(basename(__FILE__), 'seo_imagebox_metabox_nonce');
     $html = '';
     if ($image_id) {
-        $html .= '<div class="zbox" id="zbox0"><input id="image_title" name="image_title0" placeholder="Заголовок картинки"/> <input type="button" value="up"> <input type="button" value="down"> ';
+        $html .= '<div class="zbox" id="zbox0"><input id="image_title" name="image_title0" placeholder="Заголовок картинки"/> <input type="button" id="up" value="&#xf106" class="fa fa-angle-up"> <input type="button" id="down" value="&#xf107" class="fa fa-angle-down">';
         $html .= '<div></br></div>';
         $html .= '<img class="imageboxsrc" id="imageboxsrc0" src="' . $image[0] . '"/>';
         $html .= '<div><input type="hidden" class="button imagebox_image" name="imagebox_image0" value="' . $image_id . '"/><div id="zhbut"><button type="button" id="insert-media-button0" class="upload_image_button button insert-media add_media">Вставить из библиотеки</button> <button type="button" id="zhremove0" class="remove_image_button button add_media">Удалить изображение</button></div></div>';
         $html .= '<div></br></div>';
         $html .= '<input id="image_src" name="image_src0" placeholder="Ссылка"/></div>';
     } elseif (!$image_id) {
-        $html .= '<div class="zbox" id="zbox0"><input id="image_title" name="image_title0" placeholder="Заголовок картинки"/> <input type="button" value="up"> <input type="button" value="down"> ';
+        $html .= '<div class="zbox" id="zbox0"><input id="image_title" name="image_title0" placeholder="Заголовок картинки"/> <input type="button" id="up" value="&#xf106" class="fa fa-angle-up"> <input type="button" id="down" value="&#xf107" class="fa fa-angle-down">';
         $html .= '<div></br></div>';
         $html .= '<img class="imageboxsrc" id="imageboxsrc0" src=""/>';
         $html .= '<div><input type="hidden" class="button imagebox_image" name="imagebox_image0" value=""/>   <div id="zhbut"><button type="button" id="insert-media-button0" class="upload_image_button button insert-media add_media">Вставить из библиотеки</button> <button type="button" id="zhremove0" class="remove_image_button button add_media">Удалить изображение</button></div></div>';
@@ -108,35 +131,35 @@ function zh_post_editor_print_imagebox_box($post)
 }
 
 // сохранение изображения
-function zh_post_editor_save_imagebox_data($post_id)
-{
-//    var_dump($_POST);
-    // проверка, пришёл ли запрос со страницы с метабоксом
-    if (!isset($_POST['seo_imagebox_metabox_nonce']) || !wp_verify_nonce($_POST['seo_imagebox_metabox_nonce'], basename(__FILE__))) {
-        return $post_id;
-    }
-    // проверка, является ли запрос автосохранением
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-        return $post_id;
-    }
-    // проверка, права пользователя, может ли он редактировать записи
-    if (!current_user_can('edit_post', $post_id)) {
-        return $post_id;
-    }
-    // проверка типа записи и сохранение
-    $post = get_post($post_id);
-    if ($post->post_type == 'post') {
-        if (isset($_POST['imagebox_image1'])) {
-            update_post_meta($post_id, 'imagebox_image1', $_POST['imagebox_image1']);
-        } elseif (!isset($_POST['imagebox_image1'])) {
-            update_post_meta($post_id, 'imagebox_image1', null);
-        }
-    }
-
-    return $post_id;
-}
-
-add_action('save_post', 'zh_post_editor_save_imagebox_data');
+//function zh_post_editor_save_imagebox_data($post_id)
+//{
+////    var_dump($_POST);
+//    // проверка, пришёл ли запрос со страницы с метабоксом
+//    if (!isset($_POST['seo_imagebox_metabox_nonce']) || !wp_verify_nonce($_POST['seo_imagebox_metabox_nonce'], basename(__FILE__))) {
+//        return $post_id;
+//    }
+//    // проверка, является ли запрос автосохранением
+//    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+//        return $post_id;
+//    }
+//    // проверка, права пользователя, может ли он редактировать записи
+//    if (!current_user_can('edit_post', $post_id)) {
+//        return $post_id;
+//    }
+//    // проверка типа записи и сохранение
+//    $post = get_post($post_id);
+//    if ($post->post_type == 'post') {
+//        if (isset($_POST['imagebox_image1'])) {
+//            update_post_meta($post_id, 'imagebox_image1', $_POST['imagebox_image1']);
+//        } elseif (!isset($_POST['imagebox_image1'])) {
+//            update_post_meta($post_id, 'imagebox_image1', null);
+//        }
+//    }
+//
+//    return $post_id;
+//}
+//
+//add_action('save_post', 'zh_post_editor_save_imagebox_data');
 
 //// отключение перетаскивания метабоксов
 //function disable_drag_metabox() {
